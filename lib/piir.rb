810face05bdc6c59aa36c0af79260580789499f3
Coro365 <coro.365@gmail.com>
2021-09-30 10:23:16 +0900

    Change file name
    * split file piir.rb and influxdb.rb

---
+++ lib/piir.rb
@@ -0,0 +1,10 @@
+PIIR_DIR = '~/Documents/PiIR'
+IRLED_PIN = 17
+
+def send_ir(device, signal)
+  signal = signal ? 'on' : 'off'
+  signal_file = File.expand_path(File.join(PIIR_DIR, "#{device}.json"))
+  cmd = ['piir', 'play', '-g', IRLED_PIN.to_s, '-f', signal_file, signal]
+  system(*cmd)
+  Influx.post(device: device, action: signal)
+end
